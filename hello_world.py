6d83a94a8f022c15dad1691d4e0c994691f3d81e
Kader Mohideen Fasid <fasidmpm@gmail.com>
2019-11-16 11:32:33 -0800

    local host

--- hello_world.py
+++ hello_world.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-        return "Hello World from AWS ECS!"
+        return "Hello World from LocalHost!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
