2233d131c6ebee070b6d65d6c0b7abbb927fb5dc
Kader Mohideen Fasid <fasidmpm@gmail.com>
2019-11-17 10:53:36 -0800

    changed to flask-default

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-SERVICE_NAME="flask-app"
+SERVICE_NAME="flask-default"
 IMAGE_VERSION="v_"${BUILD_NUMBER}
-TASK_FAMILY="flask-app"
+TASK_FAMILY="flask-default"
 
 # Create a new task definition for this build
 sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" task-def-temp.json > task-def-temp.json-v_${BUILD_NUMBER}.json
-aws ecs register-task-definition --family flask-app --cli-input-json file://task-def-temp.json-v_${BUILD_NUMBER}.json
+aws ecs register-task-definition --family flask-default --cli-input-json file://task-def-temp.json-v_${BUILD_NUMBER}.json
 
 # Update the service with the new task definition and desired count
-TASK_REVISION=`aws ecs describe-task-definition --task-definition flask-app | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
+TASK_REVISION=`aws ecs describe-task-definition --task-definition flask-default | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
 DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} | egrep "desiredCount" | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
 if [ ${DESIRED_COUNT} = "0" ]; then
     DESIRED_COUNT="1"
